276882a3e900ba8c05525b97bcdb121d70f81142
rhemi <rhemitoth@gmail.com>
2024-07-12 17:17:58 +0200

    hi

---
+++ scripts/Queries/MockQuery.R
@@ -0,0 +1,17 @@
+## ---------------------------
+##
+## Script name: Mock Query
+##
+## Author: Rhemi Toth
+##
+## Date Created: 2024-07-12
+##
+## Email: rhemitoth@g.harvard.edu
+##
+## ---------------------------
+##
+## Notes: This is an exampe script that uses functions defined in QueryDatabseFunctions.R
+## to pull data from the database.
+##   
+##
+## ---------------------------
